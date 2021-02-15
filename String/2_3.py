641604a568f1801b412978aec04e3f3427e18ac3
Slepov, Ilya <Ilya.Slepov@gilbarco.com>
2021-02-15 12:35:38 +0400

    Task_3 fix

--- String/2_3.py
+++ String/2_3.py
@@ -1,10 +1,18 @@
 """
-3. Дана строка, состоящая из слов, разделенных пробелами. Найти самое короткое слово. Если таких слов несколько, то вывести все.
-В программе можно использовать только один цикл
+3. Дана строка, состоящая из слов, разделенных пробелами. Найти самое короткое слово. Если таких слов несколько,
+то вывести все. В программе можно использовать только один цикл
 """
 
 string = input()
 words = string.split(' ')
-words.sort(key=len)
-print(words[0])
+lengths = {}
+min_len = -1
+for word in words:
+    if lengths.__contains__(len(word)):
+        lengths[len(word)].append(word)
+    else:
+        lengths[len(word)] = [word]
+    if len(word) < min_len or min_len == -1:
+        min_len = len(word)
+print(lengths.get(min_len))
 
